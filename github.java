4f886b660b48fc4fcfd81fa455eb84bf4b3a1a41
bilal alfakih <98433009+BilalAl2021@users.noreply.github.com>
2022-09-17 20:29:23 -0400

    Update github.java

--- github.java
+++ github.java
@@ -2,7 +2,7 @@ public class github {
      public static void main(String[] args) {
          System.out.println("testing");
          System.out.println("4thtest");
-         System.out.println("5thtest");
+         System.out.println("7thtest");
          System.out.println("6thtest");
 
     }
